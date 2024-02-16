4810b369b753ea3a9a75c4aec8984eac3980084f
Ryan Nguyen <tuannguyen7326.dev@gmail.com>
2024-02-16 22:45:09 +0700

    add

--- main.ts
+++ main.ts
@@ -13,23 +13,36 @@ async function main(){
   console.log('Before.....');
 
   // Đợi 30s mới chạy chương trình. Hãy mở task manager lên và sẽ số ram.
-  await sleep(30_000);
+  await sleep(5_000);
 
   
 
 
   // Số lượng phần tử sẽ được thêm vào là 10k
-  const numberOfItems:number = 1_000_000;
+  const numberOfItems:number = 12_000;
+
+  console.log('Số phần tử là:', numberOfItems);
 
   const items:{
     id: number,
-    label: string,
+    question: string,
+    answers: string[],
+    expand: string;
   }[] = [];
 
   for(let i = 0; i < numberOfItems; i++){
     items.push({
       id: i,
-      label: `Item[${i}] New minor version of npm available! 10.2.4 -> 10.4.0`,
+      question: `Question[${i}] New minor version of npm available!
+      10.2.4 -> 10.4.0 Your branch is based on 'origin/main',
+       but the upstream is gone`,
+      answers: [
+        `Answer[${i}][1]: Your branch is based on 'origin/main', but the upstream is gone.`,
+        `Answer[${i}][2]: Your branch is based on 'origin/main', but the upstream is gone.`,
+        `Answer[${i}][3]: Your branch is based on 'origin/main', but the upstream is gone.`,
+        `Answer[${i}][4]: Your branch is based on 'origin/main', but the upstream is gone.`,
+      ],
+      expand: `Expand[${i}] Your branch is based on 'origin/main', but the upstream is gone.`,
     });
   }
 
